28b4cfeb57c2656ca378a6b0432f03ed8354f510
maroua-MEDDEB <70540750+maroua-MEDDEB@users.noreply.github.com>
2022-12-27 13:34:10 +0100

    valisdation de la formulaire,responsive

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,7 +12,6 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-
 const closeBtn = document.querySelector(".close"); // selectionner l'élément close
 const inputText1 = document.querySelector("#first");
 const inputText2 = document.querySelector("#last");
@@ -23,14 +22,12 @@ const inputRadios = document.querySelectorAll("input[type=radio]");
 const inputCondition = document.querySelector("input[type=checkbox]");
 const inputDate = document.querySelector("input[type=date]");
 
+const form = document.querySelector("form");
+// Bouton de confirmation
+const inputConfirmBtn = document.querySelector("input[type=submit]");
 
-//cibler le bouton de confirmation
-const inputConfirmBtn = document.querySelector('input[type=submit]');
-
-// cibler le modal de confirmation
-const modalConfirm = document.querySelector('.modalConfirmation');
-
-
+// Modal de confirmation
+const modalConfirm = document.querySelector(".modalConfirmation");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -39,197 +36,215 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeBtn.addEventListener("click", closeModal);
 
 // confirm bouton event
-inputConfirmBtn.addEventListener("submit", confirmButon);
- 
-//validation du champs de saisie inputText1 
-inputText1.addEventListener('input', function(event){
+form.addEventListener("submit", confirmButon);
+
+//validation du champs de saisie inputText1
+inputText1.addEventListener("input", function (event) {
   validationText1(event.target);
 });
 
 //validation du champs de saisie inputText2
-inputText2.addEventListener('input', function(event){
+inputText2.addEventListener("input", function (event) {
   validationText2(event.target);
 });
 
 //validation email
-inputEmail.addEventListener('input', function(event){
+inputEmail.addEventListener("input", function (event) {
   validationMail(event.target);
 });
 
-
 // validation nbr de concours
-inputNumber.addEventListener('input', function(event){
+inputNumber.addEventListener("input", function (event) {
   validationNumber(event.target);
 });
 
 //validation radioBox
-inputRadios.forEach(function(element){
-  element.addEventListener("input", function(event){
-  validationRadio(event.target);
-});
+inputRadios.forEach(function (element) {
+  element.addEventListener("input", function (event) {
+    validationRadio();
+  });
 });
 
 //validation checkBox
-inputCondition.addEventListener("input", function(event){
+inputCondition.addEventListener("input", function (event) {
   validationCondition(event.target);
 });
 
 //validation date
-inputDate.addEventListener("input", function(event){
+inputDate.addEventListener("input", function (event) {
   validationDate(event.target);
 });
 
 // validation btn formulaire
 inputConfirmBtn.addEventListener("click", validationForm);
 
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// 
-function confirmButon(){
-  console.log('confirmer');
-  modalbg.style.display='none';
-  modalConfirm.style.display='block';
+//
+function confirmButon() {
+  // console.log('confirmer');
+  modalbg.style.display = "none";
+  modalConfirm.style.display = "block";
 }
 
 //function Close modal
-function closeModal(){
-  modalbg.style.display = "none";
+function closeModal() {
+  //modalbg.style.display = "none";
+  resetForm();
 }
 
 // function valisationText1
-function validationText1(element){
+function validationText1(element) {
   //Chercher le parent de classe "fordata" le plus proche de "element"
-  const parentEl = element.closest('.formData');
+  const parentEl = element.closest(".formData");
 
   //On doit vérifier que l'utilisateur tape un nom valide (Un nom valide contient au moins deux caractères)
   // cas où l'utilisateur saisie moins de 2 caractères
-  if(element.value.length < 2) {
-        parentEl.setAttribute('data-error', 'Veuillez saisir votre vrai prénom!');
-        parentEl.setAttribute('data-error-visible', 'true');
-        return false;
-      }
-      else { // cas où l'utilisateur saisie plus de 2 caractères
-        parentEl.setAttribute('data-error', '');
-        parentEl.setAttribute('data-error-visible', 'false');  
-        return true;
-      }
+  if (element.value.length < 2) {
+    parentEl.setAttribute("data-error", "Veuillez saisir votre vrai prénom!");
+    parentEl.setAttribute("data-error-visible", "true");
+    return false;
+  } else {
+    // cas où l'utilisateur saisie plus de 2 caractères
+    parentEl.setAttribute("data-error", "");
+    parentEl.setAttribute("data-error-visible", "false");
+    return true;
+  }
 }
 
 //On doit vérifier que l'utilisateur tape un prenom valide
-function validationText2(element){
-  const parentEl = element.closest('.formData');
+function validationText2(element) {
+  const parentEl = element.closest(".formData");
 
-  if(element.value.length < 2) {
-        parentEl.setAttribute('data-error', 'Veuillez saisir votre vrai nom!');
-        parentEl.setAttribute('data-error-visible', 'true');
-        return false;
-      }
-      else {
-        parentEl.setAttribute('data-error', '');
-        parentEl.setAttribute('data-error-visible', 'false'); 
-        return true; 
-      }
+  if (element.value.length < 2) {
+    parentEl.setAttribute("data-error", "Veuillez saisir votre vrai nom!");
+    parentEl.setAttribute("data-error-visible", "true");
+    return false;
+  } else {
+    parentEl.setAttribute("data-error", "");
+    parentEl.setAttribute("data-error-visible", "false");
+    return true;
+  }
 }
 
 // function validation mail
-function validationMail(element){
-  const parentEl = element.closest('.formData');
+function validationMail(element) {
+  const parentEl = element.closest(".formData");
 
-  let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+  let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
 
-  if(regex.test(element.value) === false){
-    parentEl.setAttribute('data-error', 'Veuillez saisir une adresse email valide!');
-    parentEl.setAttribute('data-error-visible', 'true');
-    return true;
-  }else{
-    parentEl.setAttribute('data-error', '');
-    parentEl.setAttribute('data-error-visible', 'false');  
+  if (regex.test(element.value) === false) {
+    parentEl.setAttribute(
+      "data-error",
+      "Veuillez saisir une adresse email valide!"
+    );
+    parentEl.setAttribute("data-error-visible", "true");
     return false;
+  } else {
+    parentEl.setAttribute("data-error", "");
+    parentEl.setAttribute("data-error-visible", "false");
+    return true;
   }
-} 
+}
 
 //function validation Number
-function validationNumber(element){
-  const parentEl = element.closest('.formData');
+function validationNumber(element) {
+  const parentEl = element.closest(".formData");
 
-  if(element.value > 0){
-    parentEl.setAttribute('data-error', '');
-    parentEl.setAttribute('data-error-visible', 'false');
-    return true;  
-  }else{
-    parentEl.setAttribute('data-error', 'Veuillez saisir un nombre supérieur à 0!');
-    parentEl.setAttribute('data-error-visible', 'true');
+  if (element.value > 0) {
+    parentEl.setAttribute("data-error", "");
+    parentEl.setAttribute("data-error-visible", "false");
+    return true;
+  } else {
+    parentEl.setAttribute(
+      "data-error",
+      "Veuillez saisir un nombre supérieur à 0!"
+    );
+    parentEl.setAttribute("data-error-visible", "true");
     return false;
   }
 }
 
 //function validation radioBox
-function validationRadio(element, index){
-  let isCheked  = false;
-  inputRadios.forEach((element) => {
-    
-   if(element.checked) {
-    isCheked = true;
-    }
+function validationRadio() {
+  let isCheked;
+  let parentEl;
 
-    const parentEl = element.closest('.formData');
-    if(isCheked === true){
-      parentEl.setAttribute('data-error', '');
-      parentEl.setAttribute('data-error-visible', 'false'); 
-      return true; 
-    }else{
-      parentEl.setAttribute('data-error', 'Veuillez choisir un tournoi!');
-      parentEl.setAttribute('data-error-visible', 'true');
-      return false;
+  inputRadios.forEach((elem) => {
+    parentEl = elem.closest(".formData");
+
+    if (elem.checked) {
+      isCheked = true;
     }
-  }); 
+  });
+
+  if (isCheked) {
+    parentEl.setAttribute("data-error", "");
+    parentEl.setAttribute("data-error-visible", "false");
+    return true;
+  } else {
+    parentEl.setAttribute("data-error", "Veuillez choisir un tournoi!");
+    parentEl.setAttribute("data-error-visible", "true");
+    return false;
+  }
 }
 //function validation checkValidation
-function validationCondition(element){
-  const parentEl = element.closest('.formData');
-    if(element.checked){
-      parentEl.setAttribute('data-error', '');
-      parentEl.setAttribute('data-error-visible', 'false'); 
-      return true;
-    }else{
-      parentEl.setAttribute('data-error', 'Veuillez accepter les conditions générales!');
-      parentEl.setAttribute('data-error-visible', 'true');
-      return false;
-    } 
+function validationCondition(element) {
+  const parentEl = element.closest(".formData");
+  if (element.checked) {
+    parentEl.setAttribute("data-error", "");
+    parentEl.setAttribute("data-error-visible", "false");
+    return true;
+  } else {
+    parentEl.setAttribute(
+      "data-error",
+      "Veuillez accepter les conditions générales!"
+    );
+    parentEl.setAttribute("data-error-visible", "true");
+    return false;
+  }
 }
 
 let today = new Date().toISOString().substring(0, 10); // déclarer la date actuelle
 
 //fucntion validation Date
 function validationDate(element) {
-  const parentEl = element.closest('.formData');
-const dateOfBirth = element.value ; // la date de naissance saisie
+  const parentEl = element.closest(".formData");
+  const dateOfBirth = element.value; // la date de naissance saisie
 
-  if(dateOfBirth === '') {
-    parentEl.setAttribute('data-error', 'Veuillez saisir votre date de naissance!');
-    parentEl.setAttribute('data-error-visible', 'true');
+  if (dateOfBirth === "") {
+    parentEl.setAttribute(
+      "data-error",
+      "Veuillez saisir votre date de naissance!"
+    );
+    parentEl.setAttribute("data-error-visible", "true");
     return false;
-  } else if( today == dateOfBirth){
-    parentEl.setAttribute('data-error', 'Veuillez vérifier votre date de naissance!');
-    parentEl.setAttribute('data-error-visible', 'true');
+  } else if (today == dateOfBirth) {
+    parentEl.setAttribute(
+      "data-error",
+      "Veuillez vérifier votre date de naissance!"
+    );
+    parentEl.setAttribute("data-error-visible", "true");
     return false;
-  }else if( today < dateOfBirth){
-    parentEl.setAttribute('data-error', 'Veuillez vérifier votre date de naissance!');
-    parentEl.setAttribute('data-error-visible', 'true');
+  } else if (today < dateOfBirth) {
+    parentEl.setAttribute(
+      "data-error",
+      "Veuillez vérifier votre date de naissance!"
+    );
+    parentEl.setAttribute("data-error-visible", "true");
     return false;
-  }else if( dateOfBirth < today){
-    parentEl.setAttribute('data-error', '');
-    parentEl.setAttribute('data-error-visible', 'false'); 
+  } else if (dateOfBirth < today) {
+    parentEl.setAttribute("data-error", "");
+    parentEl.setAttribute("data-error-visible", "false");
     return true;
   }
 }
 
 // function validation formulaire
-function validationForm(e){
+function validationForm(e) {
   e.preventDefault(); //pas valider le formulaire
   // const isInputValidate = validationInput(inputText);
   const isInputText1 = validationText1(inputText1);
@@ -237,22 +252,43 @@ function validationForm(e){
 
   const isMailValidate = validationMail(inputEmail);
   const isNumberValidate = validationNumber(inputNumber);
-  const isRadioValidate = validationRadio(inputRadios);
+  const isRadioValidate = validationRadio();
   const isCheckboxValidate = validationCondition(inputCondition);
   const isDateValidate = validationDate(inputDate);
 
-  console.log(isInputText1);
+  /*console.log(isInputText1);
   console.log(isInputText2);
-  console.log(isInputText2);
+  console.log(isMailValidate);
+  console.log(isDateValidate);
   console.log(isNumberValidate);
   console.log(isRadioValidate);
-  console.log(isCheckboxValidate);
-  console.log(isDateValidate);
+  console.log(isCheckboxValidate);*/
 
-  if( isInputText1 == true && isInputText2 == true && isInputText2 == true && isNumberValidate == true && isRadioValidate == true && isCheckboxValidate == true && isDateValidate == true){
+  if (
+    isInputText1 == true &&
+    isInputText2 == true &&
+    isMailValidate == true &&
+    isDateValidate == true &&
+    isNumberValidate == true &&
+    isRadioValidate == true &&
+    isCheckboxValidate == true
+  ) {
     // afficher message de confirmation
-    modalConfirm.style.display = 'flex'; // afficher le modal de confirmation
-    modalbg.style.display='none'; // desafficher le modal formulaire
+    modalConfirm.style.display = "flex"; // afficher le modal de confirmation
+    inputConfirmBtn.setAttribute("disabled", "true");
   }
 }
 
+//L'état du formulaire par défaut (Naturellement avant l'intervention de l'utilisateur)
+function resetForm() {
+  // inputText1.value = null;
+  // inputText2.value = null;
+  // inputEmail.value = null;
+  // inputNumber.value = null;
+  // isRadioValidate = false;
+  // isCheckboxValidate = false;
+  // inputDate.value = null;
+  form.reset();
+  modalConfirm.style.display = "none";
+  inputConfirmBtn.removeAttribute("disabled");
+}
